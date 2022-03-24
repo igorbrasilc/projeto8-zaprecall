3268c463a6911186f1192ff8c940a5bd1a413c8c
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-03-24 16:50:18 -0300

    Preparando revisão de código

--- src/components/FlashcardScreen/QuestionHidden.js
+++
@@ -1,9 +0,0 @@
-export default function QuestionHidden(props) {
-    const {callback, number} = props;
-    return (
-        <button className="btn-question" onClick={() => callback(true)}>
-            <p>Pergunta {number}</p>
-            <ion-icon name="play-outline"></ion-icon>
-        </button>
-    )
-}
